543182c40d34755ad7d74c845be600e0df776739
John Holbrook <john@JHolbrook-MacBook-Pro.local>
2016-12-07 16:36:05 -0500

    create all the channel pages; begin work on adding new pins to pages

--- pinbot.py
+++ pinbot.py
@@ -1,11 +1,32 @@
 from slackclient import SlackClient
 from datetime import datetime
 import time
+import os.path
 
 
 token = open("slack_token.dat", "r").read()
 print token
 sc = SlackClient(token)
+
+def create_empty_page(channel_id):
+	channel_name = sc.api_call("channels.info", channel=channel_id)["channel"]["name"]
+	page_content = """
+<html>
+	<head>
+		<title>Frendgroop pinned messages | #%s</title>
+		<link rel="stylesheet" type="text/css" href="slack.css">
+	</head>
+	<body>
+		<h1>Pinned messages archive for #%s</h1>
+		<h4>Most recently-pinned messages displayed first</h4>
+
+		<!-- New messages below this line -->
+
+	</body>
+</html>""" % (channel_name, channel_name)
+	with open(channel_id + ".html", 'w') as f:
+		f.write(page_content)
+
 
 def generate_html_item(item):
 	channel_id = item["channel_id"]
@@ -60,6 +81,32 @@ def generate_html_item(item):
 
 	return entry
 
+def add_pin_to_file(item):
+	channel_id = item["channel_id"]
+	file_path = channel_id + ".html"
+
+	#create the file if it doesn't already exist
+	if not os.path.isfile(file_path):
+		create_empty_page(channel_id)
+
+
+	new_file = ""
+	with open(file_path, 'rw') as f:
+		lines = f.readlines()
+		counter = 0
+		while lines[0] != "		<!-- New messages below this line -->\n" and counter < len(lines):
+			new_file += lines[counter]
+			lines.remove(lines[0])
+		new_file += lines[counter]
+
+		new_file += generate_html_item(item)
+
+		while len(lines) > 0:
+			new_file += lines[0]
+			lines.remove(lines[0])
+	return new_file
+
+
 
 def generate_slack_message(item):
 	channel_id = item["channel_id"]
@@ -87,7 +134,7 @@ if sc.rtm_connect():
 			if item["type"] == "pin_added":
 				print generate_slack_message(item)
 				print item["item"]["message"]["text"]
-				print generate_html_item(item)
+				print add_pin_to_file(item)
 				#print item
 else:
 	print "connection failed"
--- utilities/get_channel_ids.py
+++ utilities/get_channel_ids.py
@@ -5,5 +5,6 @@ sc = SlackClient(token)
 if sc.rtm_connect():
 	result = sc.api_call("channels.list")
 	for channel in result["channels"]:
-		print "Here are some channels"
 		print channel["name"] + ": " + channel["id"]
+		with open(channel + ".html", 'r') as f:
+			if (f.read = )
